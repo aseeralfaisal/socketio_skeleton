d6ed78450cddaf072bc06486f5592b7629ad3dd1
aseeralfaisal <aseeralfaisal@gmail.com>
2022-01-13 22:04:27 +0600

    updatd 13-jan

--- index.js
+++ index.js
@@ -5,19 +5,21 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-const PORT = 3000 || process.env.PORT
+const PORT = 5000 || process.env.PORT
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
+  console.log("A user connected")
+  socket.on('disconnect', (socket) => {
+    console.log("A user disconnected")
+  });
   socket.on('chat message', (msg) => {
-    console.log(msg)
-    io.emit('chat message', msg);
+    io.emit('chat message', msg)
   });
 });
-
 
 server.listen(PORT, () => {
   console.log(`listening on ${PORT}`);
